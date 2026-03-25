72134ffded7f63004d38ff152e397301fe64e7d5
ytyaru <yttry0@gmail.com>
2026-03-25 18:50:41 +0900

    Created a repository

---
+++ docs/0/js/scene/decision.js
@@ -0,0 +1,12 @@
+class DecisionSceneElement extends SceneElement {
+    constructor(onNext) {
+        super();
+        this._={onNext};
+        this.#make();
+    }
+    #make() {
+        this.appendChild(Dom.tags.h1('HP:1'));
+        this.appendChild(Dom.tags.button({onclick:(e)=>this._.onNext()}, '死亡'));
+    }
+}
+customElements.define('decision-scene-element', DecisionSceneElement);
---
+++ docs/0/js/scene/init.js
@@ -0,0 +1,15 @@
+class InitSceneElement extends SceneElement {
+    constructor(onNext) {
+        super();
+        this._={onNext};
+        this.#make();
+    }
+    #make() {
+        this.appendChild(Dom.tags.h1('RougeLikeGame'));
+        //this.appendChild(Dom.tags.button({onclick:(e)=>this._.onNext()}, '開始'));
+        //this.appendChild(Dom.tags.button({onclick:(e)=>alert('click!')}, '開始'));
+        this.appendChild(Dom.tags.button({onclick:(e)=>console.log('click!')}, '開始'));
+    }
+}
+
+customElements.define('init-scene-element', InitSceneElement);
